3c12f18eb222d6ef04155197b3bef916cf8a8bbe
Ady Sanjaya <adysanjaya@outlook.co.id>
2024-01-20 11:36:29 +0800

    Initial Commit

---
+++ component/componentApp.swift
@@ -0,0 +1,17 @@
+//
+//  componentApp.swift
+//  component
+//
+//  Created by Kioser PC on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct componentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
